4b3fff160716b061e349856c2694954831ed8642
Hana-Mahmoud <76908478+Hana-Mahmoud@users.noreply.github.com>
2023-03-14 03:34:38 +0200

    'update'

--- src/Book-shelf/FilteredBooks.tsx
+++ src/Book-shelf/FilteredBooks.tsx
@@ -4,6 +4,10 @@ import { useAppSelector } from '../store/bookSlice';
 
 const FilteredBooks = (props: any) => {
   const filteredBookList: any = useAppSelector<any>((state) => state?.books?.searchResultBooks);
+  const allBooks: any = useAppSelector<any>((state) => state?.books?.books);
+  const chosenBooksFiltered = allBooks.filter((book:any) => filteredBookList.find((book2:any) => book.id === book2.id )) 
+  const mergedBooks= filteredBookList.map((obj:any) => chosenBooksFiltered.find((o:any) => o.id === obj.id) || obj);
+
 
     return (
         <div>
@@ -15,7 +19,7 @@ const FilteredBooks = (props: any) => {
         {props.books && 
       <div className="search-books-results">
       <ol className="books-grid">
-        {filteredBookList.map((b:any)=>(
+        {mergedBooks.map((b:any)=>(
          <li key={b.id}>
          <Book book={b}/>
          </li>
